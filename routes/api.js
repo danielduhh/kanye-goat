3d7e2b70cb8147cd52cbcc6bd028f03da2f9d992
Daniel Baah <dbaah@spatialdev.com>
2016-09-04 23:04:45 -0700

    remove unused package

--- routes/api.js
+++ routes/api.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 var pg = require('../pg');
 var Q = require('q');
-var ip = require('ip');
 var getmac = require('getmac');
 
 router.post('/songs', function(req,res,next){
